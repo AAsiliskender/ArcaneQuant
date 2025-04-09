960963f1d28b573a29c67a90178e6e5a5e83f9c9
Ahmed Asiliskender <90631814+AAsiliskender@users.noreply.github.com>
2025-04-09 20:15:28 +0100

    Initial Git commit for ArcaneQuant (previously AlgoFrame) project

---
+++ tests/test_SQLIntegration.py
@@ -0,0 +1,7 @@
+
+
+# SQL commands to create test user with necessary functions to be able to run tests (how should we do this for other users)?
+
+# CREATE USER "testUser" 
+# ALTER USER "testUser" WITH PASSWORD 'testPassword';
+# ALTER DATABASE "testDatabase" OWNER TO "testUser";
